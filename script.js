4e2cc279265192361204c79313d3c496e9abcb3c
Junior <junior.snk09@gmail.com>
2023-01-25 18:22:57 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+var etanol,gasolina;
+
+function calcular(){
+    etanol = parseFloat(frmFlex.txtEtanol.value.replace(",","."));
+    gasolina = parseFloat(frmFlex.txtGasolina.value.replace(",","."));
+
+    if(etanol == "" || isNaN(etanol) || gasolina == "" || isNaN(gasolina)){
+        return;
+    }
+    if ( etanol < 0.7 * gasolina) {
+        document.getElementById("status") .src="./assets/etanol.png";
+    } 
+    
+    else{
+        document.getElementById("status") .src="./assets/gasolina.png";
+    }
+}
+
+function resetar(){
+    document.getElementById("status") .src="./assets/neutro.png";
+}
+
